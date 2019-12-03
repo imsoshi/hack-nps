4193fa96736a04a729d8144164ad48184f0f1f29
imsoshi <perez.joelj@gmail.com>
2019-12-03 17:54:55 +0100

    JP

--- nps.sql
+++ nps.sql
@@ -24,3 +24,16 @@ INSERT INTO user (id, uname, fname, lname, mobile, email, password)
   VALUES (1, 'ninjapay', 'ninja', 'pay', '+31123456789', 'pay@ninja.com', 'pass1234');
 
 INSERT INTO hibernate_sequence (next_val) VALUES (2);
+
+
+
+INSERT INTO user (id, uname, fname, lname, mobile, email, password,baccount, validated,linked)
+  VALUES (2, 'ninjapay2', 'ninja2', 'pay2', '+31987654321', 'pay2@ninja.com', 'ninja1234', 'IBAN123456', 1, 1);
+INSERT INTO user (id, uname, fname, lname, mobile, email, password,baccount,linked)
+  VALUES (3, 'ninjajoe', 'ninja', 'joe', '+31112233445', 'joe@ninja.com', 'joe1234','IBAN654321',1);
+UPDATE hibernate_sequence set next_val = 4;
+
+
+INSERT INTO user (id, uname, fname, lname, mobile, email, password,baccount,linked)
+  VALUES (4, 'jp', 'Joel', 'Perez', '++31655926724', 'joel@ing.com', 'isthisastrongpassword?','IBAN098765',1);
+UPDATE hibernate_sequence set next_val = 5;
--- src/main/java/com/ninja/ninjapay/config/FfdcConfig.java
+++ src/main/java/com/ninja/ninjapay/config/FfdcConfig.java
@@ -2,7 +2,6 @@ package com.ninja.ninjapay.config;
 
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,104 +10,61 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
-import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
-import org.springframework.security.oauth2.client.token.auth.DefaultClientAuthenticationHandler;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
-import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.store.jwk.JwkTokenStore;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.access.ExceptionTranslationFilter;
-import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsAccessTokenProvider;
+import org.springframework.security.oauth2.client.token.grant.client.ClientCredentialsResourceDetails;
+
+import com.ninja.ninjapay.api.JwtClientAuthenticationHandler;
 
 @Configuration
 @EnableWebSecurity
-@EnableOAuth2Client
-public class FfdcConfig extends WebSecurityConfigurerAdapter{
-	
-	@Autowired
-    private OAuth2ClientContext oauth2ClientContext;
+public class FfdcConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private OAuth2ClientContextFilter oauth2ClientContextFilter;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll();
+    }
+
+    @Value("${auth.strong}")
+    private boolean authStrong;
+
+    @Value("${auth.keyId}")
+    private String keyId;
 
     @Value("${ffdcapi.loginUrl}")
     private String aud;
 
-    @Value("${oauth2.callbackPath}")
-    private String oauth2CallbackPath;
-
-    @Value("${oauth2.jwkSetUri}")
-    private String jwkSetUri;
-    
     @Bean
     @ConfigurationProperties("finastra.oauth2.client")
-    public AuthorizationCodeResourceDetails oAuthDetails() {
-        return new AuthorizationCodeResourceDetails();
+    public ClientCredentialsResourceDetails oAuthDetails() {
+        return new ClientCredentialsResourceDetails();
     }
-    
+
     @Bean
     public OAuth2RestTemplate restTemplate() {
-        return clientSecretRestTemplate();
+        return authStrong ? clientJwtRestTemplate() : clientSecretRestTemplate();
     }
-    
+
     private OAuth2RestTemplate clientSecretRestTemplate(){
-        OAuth2RestTemplate restTemplate = new  OAuth2RestTemplate(oAuthDetails(), oauth2ClientContext);
-        AuthorizationCodeAccessTokenProvider accessTokenProvider = new AuthorizationCodeAccessTokenProvider ();
-        accessTokenProvider.setAuthenticationHandler(new DefaultClientAuthenticationHandler());
+        return new OAuth2RestTemplate(oAuthDetails());
+    }
+
+    private OAuth2RestTemplate clientJwtRestTemplate() {
+        OAuth2RestTemplate restTemplate = new OAuth2RestTemplate(oAuthDetails());
+        ClientCredentialsAccessTokenProvider accessTokenProvider = new ClientCredentialsAccessTokenProvider();
+        accessTokenProvider.setAuthenticationHandler(jwtClientAuthenticationHandler());
         restTemplate.setAccessTokenProvider(accessTokenProvider);
         CloseableHttpClient httpClient = HttpClientBuilder.create().useSystemProperties().build();
         restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory(httpClient));
         restTemplate.setRetryBadAccessTokens(false);
         return restTemplate;
     }
-    
-    @Bean
-    public DefaultTokenServices tokenService (){
-        DefaultTokenServices services = new DefaultTokenServices();
-        services.setTokenStore(tokenStore());
-        return services;
+
+    private JwtClientAuthenticationHandler jwtClientAuthenticationHandler() {
+        JwtClientAuthenticationHandler authenticationHandler = new JwtClientAuthenticationHandler();
+        authenticationHandler.setKeyId(keyId);
+        authenticationHandler.setAud(aud);
+        authenticationHandler.afterPropertiesSet();
+        return authenticationHandler;
     }
-    
-    @Bean
-    public OAuth2ClientAuthenticationProcessingFilter oauth2ClientAuthenticationProcessingFilter() {
-        OAuth2ClientAuthenticationProcessingFilter filter =
-                new OAuth2ClientAuthenticationProcessingFilter(oauth2CallbackPath);
-        filter.setRestTemplate(restTemplate());
-        filter.setTokenServices(tokenService());
-        return filter;
-    }
-    
-    @Bean
-    public AuthenticationEntryPoint authenticationEntryPoint() {
-        return new LoginUrlAuthenticationEntryPoint(oauth2CallbackPath);
-    }
-    
-    @Bean
-    public JwkTokenStore tokenStore() {
-        return new JwkTokenStore(jwkSetUri);
-    }
-    
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-    	http.csrf().disable();
-        http.exceptionHandling()
-                .authenticationEntryPoint(authenticationEntryPoint())
-                .and()
-                .authorizeRequests()
-                .antMatchers("/results").authenticated()
-                .anyRequest().permitAll()
-                .and()
-                .addFilterAfter(oauth2ClientContextFilter, ExceptionTranslationFilter.class)
-                .addFilterBefore(oauth2ClientAuthenticationProcessingFilter(), FilterSecurityInterceptor.class)
-                .logout()
-                .logoutUrl("/appLogout")
-                .logoutSuccessUrl("/logout");
-    }
-    
 }
